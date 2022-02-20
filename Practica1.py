9875d7a1003697de972dff4a9138d365a60a619e
KritianWhite <88564832+KritianWhite@users.noreply.github.com>
2022-02-19 20:27:05 -0600

    Add files via upload

--- Practica1.py
+++ Practica1.py
@@ -1,8 +1,11 @@
+
 from tkinter import *
 from tkinter import filedialog
-from matplotlib import colors
+import webbrowser
+from matplotlib import axis, colors
 import matplotlib.pyplot as at
-import re
+import numpy as np
+
 
 # Funcion del Menú
 
@@ -60,6 +63,9 @@ def validacion():
 
 nombreProducto = []
 Cantidad_Producto = []
+arrayProducto = []
+nombreProducto = []
+ArrayAuxiliar = []
 
 def Analizar():
     # -------------------------LEER ARCHIVO .data------------------------
@@ -78,6 +84,8 @@ def Analizar():
     global Cantidad_Producto
     Cantidad_Producto = []
     cantidadProducto = False
+    global GananciaProducto
+    GananciaProducto = []
 
     Contador = 0
     auxiliar = ""
@@ -119,6 +127,7 @@ def Analizar():
 
                 listaData[1] = float(listaData[1])
                 listaData[2] = int(listaData[2])
+
                 arrayProducto.append(listaData)
                 precioProducto = False
                 cantidadProducto = False
@@ -146,15 +155,33 @@ def Analizar():
         nombreProducto.append(x[0])
         Precio_Producto.append(x[1])
         Cantidad_Producto.append(x[2])
-        #print("hello")
+        GananciaProducto.append(x[1]*x[2])
+    #print(GananciaProducto)
     
+    arrayAux = []
+    global ArrayAuxiliar
+    ArrayAuxiliar = []
+    for r in range(len(arrayProducto)):
+        arrayAux = []
+        arrayAux.append(nombreProducto[r])
+        arrayAux.append(Precio_Producto[r])
+        arrayAux.append(Cantidad_Producto[r])
+        arrayAux.append(GananciaProducto[r])
+        ArrayAuxiliar.append(arrayAux)
+    #print(ArrayAuxiliar)
+
+    #arrayProducto.sort(reverse=True)
+
     if Diccionario["grafica"] == ' "barras"':
         graficaBarras(nombreProducto, Cantidad_Producto)
+        print("Se imprimió y guardó la grafica de barras.\n")
         #print("hola")
     elif Diccionario["grafica"] == ' "lineas"':
         graficaLineas(nombreProducto, Cantidad_Producto)
+        print("Se imprimió y guardó la grafica de Lineas.\n")
     elif Diccionario["grafica"] == ' "circular"' or Diccionario["grafica"] == ' "pie"' or Diccionario["grafica"] == ' "pastel"':
         graficaCircular(nombreProducto, Cantidad_Producto)
+        print("Se imprimió y guardó la grafica circular/pie/pastel.\n")
     else:
         print("Ocurrio algún error. \n")
 
@@ -226,20 +253,291 @@ def graficaCircular(ejexB, ejeyB):
 
 def Reportes():
     
-    f = open('holamundo.html','w')
+    j = ""
+    k = ""
+    l = ""
+
+    f = open('index.html','w')
 
     mensaje = """
     
-    
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>REPORTES</title>
+    <meta name="description" content="" />
+    <meta name="author" content="Tooplate" />
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Additional CSS Files -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="assets/css/tooplate-style.css" />
+    <link rel="stylesheet" href="assets/css/owl.css" />
+    <link rel="stylesheet" href="assets/css/lightbox.css" />
+  </head>
+<!--
+https://www.tooplate.com/view/2116-blugoon
+-->
+  <body>
+    <div id="page-wraper">
+      <!-- Sidebar Menu -->
+      <div class="responsive-nav">
+        <i class="fa fa-bars" id="menu-toggle"></i>
+        <div id="menu" class="menu">
+          <i class="fa fa-times" id="menu-close"></i>
+          <div class="container">
+            <div class="image">
+              <a href="#"><img src="assets/images/logo-image.jpg" alt="Blugoon by Tooplate" /></a>
+            </div>
+            <div class="author-content">
+              <h4>202000173</h4>
+              <span>Christian Alessander Blanco González</span>
+            </div>
+            <nav class="main-nav" role="navigation">
+              <ul class="main-menu">
+                <li><a href="#section1">Introducción</a></li>
+                <li><a href="#section2">Ganancias</a></li>
+                <li><a href="#section4">Contacto</a></li>
+              </ul>
+            </nav>
+            <div class="social-network">
+              <ul class="soial-icons">
+                <li>
+                  <a href="https://www.facebook.com/KriitianWhite/"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/blanco5321"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-dribbble"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-rss"></i></a>
+                </li>
+              </ul>
+            </div>
+            <div class="copyright-text">
+              <p>Copyright 2022 Christian®<br>
+              Design: by Christian Blanco</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <section class="section about-me" data-section="section1">
+        <div class="container">
+        <div class="top-header">
+        	<img src="assets/images/aerobic-girls.jpg" alt="aerobic girls" />
+        </div>
+          <div class="section-heading">
+          
+            <h2>Sobre Nosotros</h2>
+            <div class="line-dec"></div>
+            <span>Somos una agencia creativa estratégica de propiedad independiente, siempre curiosa y lista para transformar la forma en que se hacen los negocios. Aunque somos una máquina bien engrasada, nuestra gente está lejos de ser engranajes. El talento que cultivamos adquiere la importancia de perfeccionar sus respectivos oficios. Ayuda a servir mejor tanto a los demás como a nuestros socios, y se nota en todo, desde lo que diseñamos y producimos, hasta lo que valoramos y creemos.</span>
+          </div>
+          <div class="right-image-post">
+            <div class="row">
+ 
+              <div class="col-md-8">
+                <div class="left-text">
+                  <h4>Web Design</h4>
+                  <p>No importa Estamos demasiado ocupados pensando más allá de las limitaciones del resumen típico. Creatividad: es lo que nos impulsa. Hacemos nuestro mejor trabajo cuando las barandillas están bajas y podemos extender nuestras alas. Los diseños son más limpios, las estrategias más nítidas y, en general, el mundo se convierte en un lugar mejor.</p>
+                  <p>También creemos que los grandes productos pueden venir de cualquier parte. Por lo tanto, alentamos a nuestro equipo a relajarse, estirar las piernas y explorar espacios de trabajo alternativos.
+                  </p>
+                  
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="right-image">
+                  <img src="assets/images/mountain-reflection.jpg" alt="Mountain Reflection" />
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          <div class="left-image-post">
+            <div class="row">
+            
+              <div class="col-md-4">
+                <div class="left-image">
+                  <img src="assets/images/girl-nature.jpg" alt="Nature Girl" />
+                </div>
+              </div>
+              
+              <div class="col-md-8">
+                <div class="right-text">
+                  <h4>Consultores Creativos</h4>
+                  <p>Brindamos asesoramiento estratégico y experiencia en marcas, marketing y comunicaciones para ayudar a las empresas inteligentes a lanzarse, transformarse, conectarse y prosperar.</p>
+                  <p>“Recomiendo Alto a cualquiera que busque un excelente socio de comunicación estratégica y branding”
+                    Jefe de Marketing de ASQA</p>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section my-services" data-section="section2">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Tabla de Ganancias</h2>
+            <div class="line-dec"></div>
+            <span>En la siguiente se presentan cada una de las ganancias del mes.</span>
+          </div> 
+          <TABLE align="center" border=solid>
+            <TR align="center">
+                <TH>Producto</TH>
+                <TH>Precio</TH>
+                <TH>Cantidad</TH>
+                <TH>Total Ganancias</TH>
+            </TR>"""
+    
+    f.write(mensaje)
+
+    for q in range(len(ArrayAuxiliar)):
+        j = f"""
+                <TR ALIGN=center>
+                <TD ALIGN="center">{ArrayAuxiliar[q][0]}</TD>
+                <TD ALIGN="center">{ArrayAuxiliar[q][1]}</TD>
+                <TD ALIGN="center">{ArrayAuxiliar[q][2]}</TD>
+                <TD ALIGN="center">{ArrayAuxiliar[q][1]*ArrayAuxiliar[q][2]}</TD>
+            </TR>"""
+        f.write(j)
+
+    k = """
+        </TABLE>
+        </div>
+      </section>
+
+      <section class="section contact-me" data-section="section4">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Contactanos</h2>
+            <div class="line-dec"></div>
+            <span>Para mayor información y resolución sobre tus dudas. </span>
+          </div>
+          <div class="row">
+            <div class="right-content">
+              <div class="container">
+                <form id="contact" action="" method="post">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="name" type="text" class="form-control"
+                          id="name" placeholder="Your name..." required="" />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="email" type="text" class="form-control"
+                          id="email" placeholder="Your email..." required=""  />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <input name="subject" type="text" class="form-control" 
+                          id="subject" placeholder="Subject..." required="" />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <textarea name="message" rows="6" class="form-control"
+                          id="message" placeholder="Your message..." required="" ></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="button">
+                          Enviar Mensaje
+                        </button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+      //according to loftblog tut
+      $(".main-menu li:first").addClass("active");
+
+      var showSection = function showSection(section, isAnimate) {
+        var direction = section.replace(/#/, ""),
+          reqSection = $(".section").filter(
+            '[data-section="' + direction + '"]'
+          ),
+          reqSectionPos = reqSection.offset().top - 0;
+
+        if (isAnimate) {
+          $("body, html").animate(
+            {
+              scrollTop: reqSectionPos
+            },
+            800
+          );
+        } else {
+          $("body, html").scrollTop(reqSectionPos);
+        }
+      };
+
+      var checkSection = function checkSection() {
+        $(".section").each(function() {
+          var $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+          if (topEdge < wScroll && bottomEdge > wScroll) {
+            var currentId = $this.data("section"),
+              reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+            reqLink
+              .closest("li")
+              .addClass("active")
+              .siblings()
+              .removeClass("active");
+          }
+        });
+      };
+
+      $(".main-menu").on("click", "a", function(e) {
+        e.preventDefault();
+        showSection($(this).attr("href"), true);
+      });
+
+      $(window).scroll(function() {
+        checkSection();
+      });
+    </script>
+  </body>
+</html>
     """
 
-    f.write(mensaje)
+    f.write(k)
     f.close()
-    print("insertar procedimineot aqui")
+    webbrowser.open_new_tab("index.html")
+    print("Reporte impreso")
 
 
 def Salir():
+    print("\nEsperamos vuelva pronto. Con cuidado!\n")
     exit()
 
 
@@ -253,8 +551,7 @@ if opcion == 3:
     Analizar()
     Menu()
 if opcion == 4:
-    graficaCircular(nombreProducto, Cantidad_Producto)
-    # Reportes()
+    Reportes()
     Menu()
 if opcion == 5:
     Salir()
@@ -262,4 +559,4 @@ if opcion <= 0 or opcion >= 6:
     print("Error, ingrese un número del menú.\n")
     Menu()
 else:
-    print(" ")
+    print("Ocurrio un error")
